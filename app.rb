0dfe28f2be6ef35553d9edbbfe6f9291b24c7e3e
monet-jones <monet.s.jones@gmail.com>
2018-08-02 20:50:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
+  get '/' do
     erb :food_form
   end
 
+
+
+post '/food' do
+  @name = params[:username]
+  @food = params[:food]
+  @season= params[:season]
+  @dessert=params[:dessert]
+  @indoor_outdoor= params[:indoor_outdoor]
+  @ethnicity=params[:ethnicity]
+  
+  erb :food
+end 
   # Add your post route and action below
 
 end
